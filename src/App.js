eed7ef2bdc9ec18038a79f8b4b6cd17167c4a387
Nicolas Restrepo <nrestrepo.r05@gmail.com>
2021-09-01 08:17:18 -0400

    add register ui

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
-import { BrowserRouter, Route, Link } from 'react-router-dom';
+import { BrowserRouter, Route } from 'react-router-dom';
 import Home from './components/Home/Home';
 import ProductDetail from './components/ProductDetail/ProductDetail';
 import Header from './components/Header/Header';
 import Login from './components/Login/Login';
+import Register from './components/Register/Register';
 
 
 function App() {
@@ -12,11 +13,10 @@ function App() {
       <Header />
       <div className="grid-container">
         <main className="main">
-          {/* <div className="content"> */}
-            <Route path="/" exact={true} component={Home}/>
-            <Route path="/product/:id" exact={true} component={ProductDetail}/>
-            <Route path="/login" exact={true} component={Login}/>
-          {/* </div> */}
+          <Route path="/" exact={true} component={Home}/>
+          <Route path="/product/:id" exact={true} component={ProductDetail}/>
+          <Route path="/login" exact={true} component={Login}/>
+          <Route path="/register" exact={true} component={Register}/>
         </main>
         <footer className="footer">
           All right reserved.
---
+++ src/components/Register/Register.jsx
@@ -0,0 +1,19 @@
+import Form from "../Form/Form";
+import Input from "../Input/Input";
+import Button from "../Button/Button";
+
+const Register = () => {
+  return (
+    <Form title="Sign Up">
+      <Input title="Name" />
+      <Input title="Email" />
+      <Input title="Password" type="password" />
+      <Input title="Password Confirmation" type="password" />
+      <Button>
+        Sign Up!
+      </Button>
+    </Form>
+  );
+};
+
+export default Register;
