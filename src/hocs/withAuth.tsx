2f351ed5c7a958ab7f7541678e145cb7c58764c0
Nicolas Restrepo <nrestrepo.r05@gmail.com>
2021-09-09 16:48:07 -0400

    migrate withAuth to ts

--- src/hocs/withAuth.tsx
+++ src/hocs/withAuth.tsx
@@ -1,10 +1,11 @@
 import { useSelector } from "react-redux";
+import { RootState } from "../redux/rootReducer";
 
 
-const withAuth = (WrappedComponent, url) => {
-  return (props) => {
+const withAuth = (WrappedComponent: any, url: string) => {
+  return (props: any) => {
     if (typeof window !== "undefined") {
-      const token = useSelector(state => state.auth.token)
+      const token = useSelector((state: RootState) => state.auth.token)
       if (!token) {
         window.location.assign(url ? url : "/login");
         return null;
