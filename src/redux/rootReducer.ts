726ce5351e5027f182019149c8940fa967c6d574
Nicolas Restrepo <nrestrepo.r05@gmail.com>
2021-09-09 11:05:00 -0400

    migrate rootReducer to ts

---
+++ src/redux/rootReducer.ts
@@ -0,0 +1,14 @@
+import { combineReducers } from '@reduxjs/toolkit';
+
+import cartReducer from "./cart/reducer";
+import productReducer from "./product/reducer";
+import authReducer from "./auth/reducer";
+
+const rootReducer = combineReducers({
+    productList: productReducer,
+    cart: cartReducer,
+    auth: authReducer,
+});
+
+export type RootState = ReturnType<typeof rootReducer>
+export default rootReducer;
